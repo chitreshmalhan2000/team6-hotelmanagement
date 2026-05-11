afa51ae3b222d57a9d71332412c8a07f7f4d9a32
Aditya Bhardwaj <973adityabhardwaj@gmail.com>
2026-05-11 21:43:57 +0530

    Initial Commit -> Added: Scaffolded tables from databse to Models.

---
+++ HotelManagement.Common/Data/HotelDbContext.cs
@@ -0,0 +1,217 @@
+﻿using HotelManagement.Common.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelManagement.Common.Data;
+
+public partial class HotelDbContext : DbContext
+{
+    public HotelDbContext() { }
+
+    public HotelDbContext(DbContextOptions<HotelDbContext> options)
+        : base(options) { }
+
+    public virtual DbSet<Amenity> Amenities { get; set; }
+
+    public virtual DbSet<Hotel> Hotels { get; set; }
+
+    public virtual DbSet<Payment> Payments { get; set; }
+
+    public virtual DbSet<Reservation> Reservations { get; set; }
+
+    public virtual DbSet<Review> Reviews { get; set; }
+
+    public virtual DbSet<Room> Rooms { get; set; }
+
+    public virtual DbSet<RoomType> RoomTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=hotel;User Id=sa;Password=Admin@123;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Amenity>(entity =>
+        {
+            entity.HasKey(e => e.AmenityId).HasName("pk_Amenity");
+
+            entity.ToTable("Amenity");
+
+            entity.Property(e => e.AmenityId).HasColumnName("amenity_id");
+            entity.Property(e => e.Description)
+                .IsUnicode(false)
+                .HasColumnName("description");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Hotel>(entity =>
+        {
+            entity.HasKey(e => e.HotelId).HasName("pk_Hotel");
+
+            entity.ToTable("Hotel");
+
+            entity.Property(e => e.HotelId).HasColumnName("hotel_id");
+            entity.Property(e => e.Description)
+                .IsUnicode(false)
+                .HasColumnName("description");
+            entity.Property(e => e.Location)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("location");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("name");
+
+            entity.HasMany(d => d.Amenities).WithMany(p => p.Hotels)
+                .UsingEntity<Dictionary<string, object>>(
+                    "HotelAmenity",
+                    r => r.HasOne<Amenity>().WithMany()
+                        .HasForeignKey("AmenityId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("fk_HotelAmenity_Amenity"),
+                    l => l.HasOne<Hotel>().WithMany()
+                        .HasForeignKey("HotelId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("fk_HotelAmenity_Hotel"),
+                    j =>
+                    {
+                        j.HasKey("HotelId", "AmenityId").HasName("pk_HotelAmenity");
+                        j.ToTable("HotelAmenity");
+                        j.IndexerProperty<int>("HotelId").HasColumnName("hotel_id");
+                        j.IndexerProperty<int>("AmenityId").HasColumnName("amenity_id");
+                    });
+        });
+
+        modelBuilder.Entity<Payment>(entity =>
+        {
+            entity.HasKey(e => e.PaymentId).HasName("pk_Payment");
+
+            entity.ToTable("Payment");
+
+            entity.Property(e => e.PaymentId).HasColumnName("payment_id");
+            entity.Property(e => e.Amount)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("amount");
+            entity.Property(e => e.PaymentDate).HasColumnName("payment_date");
+            entity.Property(e => e.PaymentStatus)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("payment_status");
+            entity.Property(e => e.ReservationId).HasColumnName("reservation_id");
+
+            entity.HasOne(d => d.Reservation).WithMany(p => p.Payments)
+                .HasForeignKey(d => d.ReservationId)
+                .HasConstraintName("fk_Payment_Reservation");
+        });
+
+        modelBuilder.Entity<Reservation>(entity =>
+        {
+            entity.HasKey(e => e.ReservationId).HasName("pk_Reservation");
+
+            entity.ToTable("Reservation");
+
+            entity.Property(e => e.ReservationId).HasColumnName("reservation_id");
+            entity.Property(e => e.CheckInDate).HasColumnName("check_in_date");
+            entity.Property(e => e.CheckOutDate).HasColumnName("check_out_date");
+            entity.Property(e => e.GuestEmail)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("guest_email");
+            entity.Property(e => e.GuestName)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("guest_name");
+            entity.Property(e => e.GuestPhone)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("guest_phone");
+            entity.Property(e => e.RoomId).HasColumnName("room_id");
+
+            entity.HasOne(d => d.Room).WithMany(p => p.Reservations)
+                .HasForeignKey(d => d.RoomId)
+                .HasConstraintName("fk_Reservation_Room");
+        });
+
+        modelBuilder.Entity<Review>(entity =>
+        {
+            entity.HasKey(e => e.ReviewId).HasName("pk_Review");
+
+            entity.ToTable("Review");
+
+            entity.Property(e => e.ReviewId).HasColumnName("review_id");
+            entity.Property(e => e.Comment)
+                .IsUnicode(false)
+                .HasColumnName("comment");
+            entity.Property(e => e.Rating).HasColumnName("rating");
+            entity.Property(e => e.ReservationId).HasColumnName("reservation_id");
+            entity.Property(e => e.ReviewDate).HasColumnName("review_date");
+
+            entity.HasOne(d => d.Reservation).WithMany(p => p.Reviews)
+                .HasForeignKey(d => d.ReservationId)
+                .HasConstraintName("fk_Review_Reservation");
+        });
+
+        modelBuilder.Entity<Room>(entity =>
+        {
+            entity.HasKey(e => e.RoomId).HasName("pk_Room");
+
+            entity.ToTable("Room");
+
+            entity.Property(e => e.RoomId).HasColumnName("room_id");
+            entity.Property(e => e.IsAvailable).HasColumnName("is_available");
+            entity.Property(e => e.RoomNumber).HasColumnName("room_number");
+            entity.Property(e => e.RoomTypeId).HasColumnName("room_type_id");
+
+            entity.HasOne(d => d.RoomType).WithMany(p => p.Rooms)
+                .HasForeignKey(d => d.RoomTypeId)
+                .HasConstraintName("fk_Room_RoomType");
+
+            entity.HasMany(d => d.Amenities).WithMany(p => p.Rooms)
+                .UsingEntity<Dictionary<string, object>>(
+                    "RoomAmenity",
+                    r => r.HasOne<Amenity>().WithMany()
+                        .HasForeignKey("AmenityId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("fk_RoomAmenity_Amenity"),
+                    l => l.HasOne<Room>().WithMany()
+                        .HasForeignKey("RoomId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("fk_RoomAmenity_Room"),
+                    j =>
+                    {
+                        j.HasKey("RoomId", "AmenityId").HasName("pk_RoomAmenity");
+                        j.ToTable("RoomAmenity");
+                        j.IndexerProperty<int>("RoomId").HasColumnName("room_id");
+                        j.IndexerProperty<int>("AmenityId").HasColumnName("amenity_id");
+                    });
+        });
+
+        modelBuilder.Entity<RoomType>(entity =>
+        {
+            entity.HasKey(e => e.RoomTypeId).HasName("pk_RoomType");
+
+            entity.ToTable("RoomType");
+
+            entity.Property(e => e.RoomTypeId).HasColumnName("room_type_id");
+            entity.Property(e => e.Description)
+                .IsUnicode(false)
+                .HasColumnName("description");
+            entity.Property(e => e.MaxOccupancy).HasColumnName("max_occupancy");
+            entity.Property(e => e.PricePerNight)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("price_per_night");
+            entity.Property(e => e.TypeName)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("type_name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
